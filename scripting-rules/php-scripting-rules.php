da9861dec007dad52809c85e4c3489fda4504ed5
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-08-27 17:11:22 -0700

    scripting rules and comments

---
+++ scripting-rules/php-scripting-rules.php
@@ -0,0 +1,12 @@
+// single line comment
+
+# single line comment
+
+/* Multi
+   Line
+   Comment */
+
+// simicolon indicates end of what needs to be run 
+// php works in first to last order of oporations
+<?php echo"Print this first";
+      echo"Print this second";
