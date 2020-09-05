a51ecbca4d84c94920e2b80a12f93d42a3a3a103
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-09-04 17:32:09 -0700

    notes for creating arrays

---
+++ producingArrays.php
@@ -0,0 +1,27 @@
+""" creating an array """
+// $days[] = 'Monday'; $days[] = 'Tuesday'; $days[] = 'Wednesday'; $days[] = 'Thursday'; $days[] = 'Friday';
+// or $days = array("Mondat", "Tuesday", "Wednesday", "Thursday", "Friday");
+
+// Associatave arrays
+// you can assign a key name to each element
+// $months["jan"] = "January";
+// $months["feb"] = "February";
+// $months["mar"] = "March";
+// $months["apr"] = "April";
+// $months["may"] = "May";
+// or
+// array("jan" => "January", "feb" => "February", "mar" => "March", "apr" => "April", "may" => "May")
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Embedding php in html</title>
+</head>
+<body>
+    <?php   
+    # write the traditional greeting
+    echo '<h1>Hello World!</h1>' ;
+    ?>
+</body>
+</html>
